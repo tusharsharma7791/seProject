f699f7e622bb734ca917c2204688f74670dc0bf5
tusharsharma7791 <100347623+tusharsharma7791@users.noreply.github.com>
2024-10-12 18:54:47 +0530

    cors tried

--- chatapp-server-master/constants/config.js
+++ chatapp-server-master/constants/config.js
@@ -1,7 +1,5 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
     "https://se-project-theta.vercel.app/login", // Update with your frontend URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
