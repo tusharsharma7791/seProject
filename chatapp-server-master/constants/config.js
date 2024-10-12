0a8805962022fc17d7750476022e8bdaf7abca19
tusharsharma7791 <100347623+tusharsharma7791@users.noreply.github.com>
2024-10-12 19:05:44 +0530

    url update

--- chatapp-server-master/constants/config.js
+++ chatapp-server-master/constants/config.js
@@ -1,6 +1,6 @@
 const corsOptions = {
   origin: [
-    "https://se-project-theta.vercel.app/login", // Update with your frontend URL
+    "https://se-project-drab.vercel.app/login", // Update with your frontend URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
